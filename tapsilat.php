4bbed1281b57d0f058e8ae6bdb51654090c3d66e
Özgür Yalçın <ozguryalcin@outlook.com>
2023-01-22 16:33:25 +0300

    Update tapsilat.php

--- tapsilat.php
+++ tapsilat.php
@@ -25,8 +25,8 @@ function init() {
             $this->icon = null;
             $this->has_fields = true;
             $this->title = "Credit Card/Debit Card or alternative payment methods";
-            $this->method_title = "Pay with Card/Debit Card or alternative payment methods";
-            $this->method_description = "Credit Card/Debit Card or alternative payment methods)";
+            $this->method_title = "Credit Card/Debit Card or alternative payment methods";
+            $this->method_description = "Pay with Card/Debit Card or alternative payment methods";
             $this->init_form_fields();
             $this->init_settings();
             add_action("woocommerce_receipt_" . $this->id, array($this, "receipt"));
